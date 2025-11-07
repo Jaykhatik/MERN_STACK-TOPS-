ddf246dc03c25bca802f76ebe38759f3e952d77e
Jaykumar Khatik <jaykhatik1905@gmail.com>
2025-11-07 15:44:21 +0530

    first commit

--- React/first/src/App.js
+++ React/first/src/App.js
@@ -1,10 +1,11 @@
+import Home from "./pages/Home";
 
 
 
 function App() {
   return (
     <>
-    <h1>jay</h1>
+    <Home/>
     </>
   );
 }
---
+++ React/first/src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from '../common/Header'
+import Footer from '../common/Footer'
+
+function Home() {
+  return (
+<><div>
+    <Header/>
+  <div className="container p-5 my-5 border">
+    <h1>My First Bootstrap Page</h1>
+    <p>This container has a border and some extra padding and margins.</p>
+  </div>
+  <div className="container p-5 my-5 bg-dark text-white">
+    <h1>My First Bootstrap Page</h1>
+    <p>This container has a dark background color and a white text, and some extra padding and margins.</p>
+  </div>
+  <div className="container p-5 my-5 bg-primary text-white">
+    <h1>My First Bootstrap Page</h1>
+    <p>This container has a blue background color and a white text, and some extra padding and margins.</p>
+  </div>
+</div>
+<Footer/>
+
+</>
+  )
+}
+
+export default Home
